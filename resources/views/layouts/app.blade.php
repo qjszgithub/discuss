7a752521a58097689078d2e8b3896a146f5d740d
qiujiasen <qiujiasen@vrcode.com>
2019-04-08 10:47:53 +0800

    the fundation construction

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{app()->getLocale()}}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{csrf_token()}}">
+
+    <title>@yield('title','LaraBBS')- laravel57 discuss</title>
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+
+</head>
+
+<body>
+    <div id="app" class="{{route_class()}}-page">
+        @include('layouts._header')
+
+        <div class="container">
+            @include('share._messages')
+
+            @yield('content')
+
+        </div>
+
+        @include('layouts._footer')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{mix('js/app.js')}}"></script>
+</body>
+
+</html>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title','首页')
+@section('content')
+    <h1>Trouble-sen这里是首页</h1>
+@stop
