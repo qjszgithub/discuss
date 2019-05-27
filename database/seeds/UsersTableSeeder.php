a2c75e33821a799dc54d12ee0f2b3262428de6d8
vagrant <vagrant@homestead>
2019-05-27 02:50:20 +0000

    replay list

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -44,8 +44,9 @@ class UsersTableSeeder extends Seeder
 
         // 单独处理第一个用户的数据
         $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->name = 'troublesen';
+        $user->password = bcrypt('123456');
+        $user->email = 'troublesen@example.com';
         $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
 
