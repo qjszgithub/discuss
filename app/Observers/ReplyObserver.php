e07503a07bb81a306dacf006e1ba23d5e781aa00
vagrant <vagrant@homestead>
2019-05-27 03:40:23 +0000

    notificate user have new replay

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,13 +3,14 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class ReplyObserver
 {
-    
+
 
     public function updating(Reply $reply)
     {
@@ -25,5 +26,8 @@ class ReplyObserver
     {
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 }
